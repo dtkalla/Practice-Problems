c9a3e0c262bedc90473129bfc24ab8542fffdb13
Daniel <danieltkalla@gmail.com>
2023-01-05 08:49:06 -0500

    rename problems, add first LeetCode

---
+++ LeetCode/149_max_points_on_1_line.rb
@@ -0,0 +1,47 @@
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+ 
+
+# Example 1:
+
+
+# Input: points = [[1,1],[2,2],[3,3]]
+# Output: 3
+# Example 2:
+
+
+# Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+# Output: 4
+ 
+
+# Constraints:
+
+# 1 <= points.length <= 300
+# points[i].length == 2
+# -104 <= xi, yi <= 104
+# All the points are unique.
+
+
+
+
+
+def max_points(points)
+    return points.length if points.length < 3
+    max = 2
+    points.each do |p1|
+        line = Hash.new(1)
+        points.each do |point|
+            if point == p1
+                1+1
+            elsif point[0] == p1[0]
+                line['vertical'] += 1
+            else
+                m = (point[1]-p1[1])*1.0/(point[0]-p1[0])
+                #b = point[1] - m * point[0]
+                line[[m]] += 1
+            end
+        end
+        line.each {|k,v| max = v if v > max}
+    end
+    max
+end
