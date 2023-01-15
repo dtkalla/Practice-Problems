a2c42439e911f35158ca26763aa25ed65214f2a0
Daniel <danieltkalla@gmail.com>
2023-01-15 09:12:16 -0500

    add comments in code to 695

--- LeetCode/695_max_area_of_island.rb
+++ LeetCode/695_max_area_of_island.rb
@@ -29,8 +29,8 @@ require 'set'
 
 def max_area_of_island(grid)
     max = 0
-    checked = Set[]
-    dirs = [[0,1],[0,-1],[-1,0],[1,0]]
+    checked = Set[] # check each element of grid exactly once
+    dirs = [[0,1],[0,-1],[-1,0],[1,0]] # no diagonals
     m = grid.length
     n = grid[0].length
     (0...m).each do |i|
@@ -44,7 +44,8 @@ def max_area_of_island(grid)
                     checked.add(ele)
                     dirs.each do |dir|
                         a,b = ele[0] + dir[0], ele[1] + dir[1]
-                        if valid?(m,n,a,b) && grid[a][b] != 0 && !checked.include?([a,b])
+                        if valid?(m,n,a,b) && grid[a][b] != 0 && !checked.include?([a,b]) # on the grid & part of the island & not checked yet
+                             # 6 layers deep, ooof!  I could reduce layering, but I'm not sure it would actually be more readable...
                             queue << [a,b]
                             checked.add([a,b])
                         end
@@ -59,7 +60,7 @@ end
 
 
 def valid?(m,n,a,b)
-    (0...m).cover?(a) && (0...n).cover?(b)
+    (0...m).cover?(a) && (0...n).cover?(b) # cover? is faster than include?
 end
 
 
